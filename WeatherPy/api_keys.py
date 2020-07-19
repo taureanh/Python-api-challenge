54363ac6d12cf0e0c68a1e16415565fe313c16dc
taureanh <taurean.hutchinson@gmail.com>
2020-07-19 17:21:27 -0400

    update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e24e77a3beec00053c3f86b8d112d201"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDrdmUdeZeFvF4-ojHG-IvANehhKDQniM0"
+g_key = ""
